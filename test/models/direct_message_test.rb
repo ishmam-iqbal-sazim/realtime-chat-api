d1cc581d384e4d37d982415d3c3b5efe3d2e9c81
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-12-18 14:41:33 +0600

    Add test suites for serializers and models.
    - Add files direct_message_test, user_test, message_serializer_test and
      user_serializer_test.
    - Add unit tests for each.

--- test/models/direct_message_test.rb
+++ test/models/direct_message_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class DirectMessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of(:content)
+
+  test 'message is created with validations' do
+    valid_message = DirectMessage.new(
+      content: 'Valid message',
+    )
+    assert valid_message.valid?
+    assert_empty valid_message.errors
+
+    invalid_message = DirectMessage.new(
+      content: '',
+    )
+    assert_not invalid_message.valid?
+    assert_not_empty invalid_message.errors[:content]
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of(:username)
+  should validate_presence_of(:password)
+
+  test 'user is created with validations' do
+    valid_user = User.new(
+      username: 'validUsername',
+      password: 'validPassword'
+    )
+
+    assert valid_user.valid?
+    assert_empty valid_user.errors
+
+    invalid_user = User.new(
+      username: '',
+      password: ''
+    )
+
+    assert_not invalid_user.valid?
+    assert_not_empty invalid_user.errors[:username]
+    assert_not_empty invalid_user.errors[:password]
+  end
 end
